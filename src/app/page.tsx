b06af5e5ff1f95a950779d6eaa59c081cf997088
Kyle-Ski <skiroyjenkins@gmail.com>
2025-07-17 16:42:24 -0700

    removing getElemetnById selector and using react state, fixing number.includes() bug, WIP: updating reset function, clearing input field, displaying information correclty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [advocates, setAdvocates] = useState([]);
   const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     console.log("fetching advocates...");
@@ -17,20 +18,20 @@ export default function Home() {
     });
   }, []);
 
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
+  const onChange = (e: any) => {
+    const inputValue = e.target.value;
+    setSearchTerm(inputValue);
 
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
+      const searchLower = inputValue.toLowerCase();
       return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
+        advocate.firstName.includes(searchLower) ||
+        advocate.lastName.includes(searchLower) ||
+        advocate.city.includes(searchLower) ||
+        advocate.degree.includes(searchLower) ||
+        advocate.specialties.includes(searchLower) ||
+        advocate.yearsOfExperience.toString().includes(searchLower)
       );
     });
 
@@ -50,7 +51,7 @@ export default function Home() {
       <div>
         <p>Search</p>
         <p>
-          Searching for: <span id="search-term"></span>
+          Searching for: <span id="search-term">{searchTerm}</span>
         </p>
         <input style={{ border: "1px solid black" }} onChange={onChange} />
         <button onClick={onClick}>Reset Search</button>
